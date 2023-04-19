3162f03a03663bcc8095c67f6819bbcb80fbbe13
nagt1997@gmail.com <nagt1997@gmail.com>
2023-04-19 15:35:57 +0900

    배열 자동 늘어남

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -9,12 +9,40 @@ public class MyArrayList<T> {
     }
 
     public boolean add(String element){
+        makeNewdataNotEnough();
+
         data[size] =element;
         size ++;
         return true;
     }
 
+    private void makeNewdataNotEnough() {
+        //먼저 공간이 부족한지 확인
+        if(ifNotEnoush()){
+            makeNewData();
+        }
+    }
+
+    private void makeNewData() {
+        //새 배열 생성
+        String[] newData = new String[data.length*2];
+
+        //기존 창고에 있던 물품을 새 창고로 옮긴다
+        for(int i=0; i<data.length; i++){
+            newData[i] = data[i];
+        }
+
+        //기존 창고와 계약해지우 물품 옮긴다,
+        //이전 창고 가비지 컬랙터로 제거댐
+        data =newData;
+    }
+
+    private boolean ifNotEnoush() {
+        return size >= data.length;
+    }
+
     public String get(int index) {
         return  data[index];
     }
+
 }
