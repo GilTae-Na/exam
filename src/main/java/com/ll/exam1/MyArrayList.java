94b81efc15762cdcd3fc8e6c1ec0bbd003297a1b
nagt1997@gmail.com <nagt1997@gmail.com>
2023-04-19 14:40:14 +0900

    get

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,7 +1,19 @@
 package com.ll.exam1;
 
 public class MyArrayList<T> {
+
+    private int size = 0;
+
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean add(T element){
+        size ++;
+        return true;
+    }
+
+    public String get(int index) {
+        return  "포도";
     }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -1,17 +1,42 @@
 package com.ll.exam1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.ll.exam1.MyArrayList;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyArrayListTest {
 
     @Test
-    void testSize() {
+    @DisplayName("testSize")
+    void t1() {
         MyArrayList<String> list = new MyArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list.size()).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("add/ 사과")
+    void t2() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        assertThat(list.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("get(1)")
+    void t3() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        assertThat(list.get(1)).isEqualTo("포도");
+    }
+
+
 
 }
